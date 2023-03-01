5d5d7625283141bf0540fd3cf7637ec46e87fac0
Jonathan Schweppe <jo.schweppe@gmail.com>
2023-03-01 01:32:47 +0100

    Adds src and dist folder and webp gulp converter
    Signed-off-by: Jonathan Schweppe <jo.schweppe@gmail.com>

--- themes/your-theme/gulpfile.js
+++ themes/your-theme/gulpfile.js
@@ -1,23 +1,48 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var cleancss = require('gulp-clean-css');
-var csscomb = require('gulp-csscomb');
-var rename = require('gulp-rename');
-var autoprefixer = require('gulp-autoprefixer');
-var sourcemaps = require('gulp-sourcemaps');
-var browserSync = require('browser-sync').create();
-var reload = browserSync.reload;
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const cleancss = require('gulp-clean-css');
+const csscomb = require('gulp-csscomb');
+const rename = require('gulp-rename');
+const autoprefixer = require('gulp-autoprefixer');
+const sourcemaps = require('gulp-sourcemaps');
+const browserSync = require('browser-sync').create();
+const reload = browserSync.reload;
+const webp = require('gulp-webp');
 
 // configure the paths
-var watch_dir_scss = './scss/**/*.scss';
-var watch_dir_twig = './templates/**/*.html.twig';
-var watch_dir_js = './js/**/*.js';
-var src_dir = './scss/*.scss';
-var dest_dir = './css-compiled';
 
-var paths = {
-    source: src_dir
-};
+const watch_dir_scss = './src/scss/**/*.scss';
+const watch_dir_twig = './templates/**/*.html.twig';
+const watch_dir_js = './src/js/**/*.js';
+const watch_dir_img = './src/images/**/*.{png,jpg,jpeg}';
+const src_dir_scss = './src/scss/*.scss';
+const src_dir_images = './src/images/**/*.{png,jpg,jpeg}';
+const dest_dir_images = './dist/images-compiled';
+const dest_dir_scss = './dist/css-compiled';
+
+function build() {
+  return gulp.src(src_dir_scss)
+    .pipe(sourcemaps.init())
+    .pipe(sass({
+      outputStyle: 'compact',
+      precision: 10
+    }).on('error', sass.logError))
+    .pipe(sourcemaps.write())
+    .pipe(autoprefixer())
+    .pipe(csscomb())
+    .pipe(cleancss())
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(gulp.dest(dest_dir_scss))
+    .pipe(browserSync.stream());
+}
+
+gulp.task('images', () => {
+  return gulp.src(src_dir_images)
+  .pipe(webp())
+  .pipe(gulp.dest('./dist/images-compiled'));
+});
 
 function watch() {
     browserSync.init({
@@ -28,26 +53,5 @@ function watch() {
   return gulp.watch(watch_dir_scss, build);
 }
 
-function build() {
-  return gulp.src(paths.source)
-      .pipe(sourcemaps.init())
-      .pipe(sass({
-            outputStyle: 'compact',
-            precision: 10
-          }).on('error', sass.logError)
-      )
-      .pipe(sourcemaps.write())
-      .pipe(autoprefixer())
-      .pipe(gulp.dest(dest_dir))
-      .pipe(csscomb())
-      .pipe(cleancss())
-      .pipe(rename({
-        suffix: '.min'
-      }))
-      .pipe(gulp.dest(dest_dir))
-      .pipe(browserSync.stream());
-}
-
 exports.watch = watch;
 exports.build = build;
-exports.default = build;
