ff2e468f7559c93bbe3f22bd70b891339f08bdb8
Jonathan Schweppe <jo.schweppe@gmail.com>
2023-03-02 19:53:18 +0100

    updates gulp and minor bugfixes
    Signed-off-by: Jonathan Schweppe <jo.schweppe@gmail.com>

--- themes/your-theme/gulpfile.js
+++ themes/your-theme/gulpfile.js
@@ -1,57 +1,57 @@
 const gulp = require('gulp');
-const sass = require('gulp-sass');
-const cleancss = require('gulp-clean-css');
-const csscomb = require('gulp-csscomb');
-const rename = require('gulp-rename');
-const autoprefixer = require('gulp-autoprefixer');
-const sourcemaps = require('gulp-sourcemaps');
+const sass = require('gulp-sass')(require('sass'));
 const browserSync = require('browser-sync').create();
-const reload = browserSync.reload;
 const webp = require('gulp-webp');
+const del  = require('del')
 
-// configure the paths
+const paths = {
+  src: {
+    scss: './src/scss/*.scss',
+    images: './src/images/**/*.{png,jpg,jpeg}'
+  },
+  watch: {
+    scss: './src/scss/**/*.scss',
+    images: './src/images/**/*.{png,jpg,jpeg}',
+    twig: './templates/**/*.html.twig',
+    js: './src/js/**/*.js'
+  },
+  dest: {
+    css: './dist/css-compiled',
+    images: './dist/images-compiled'
+  }
+};
 
-const watch_dir_scss = './src/scss/**/*.scss';
-const watch_dir_twig = './templates/**/*.html.twig';
-const watch_dir_js = './src/js/**/*.js';
-const watch_dir_img = './src/images/**/*.{png,jpg,jpeg}';
-const src_dir_scss = './src/scss/*.scss';
-const src_dir_images = './src/images/**/*.{png,jpg,jpeg}';
-const dest_dir_images = './dist/images-compiled';
-const dest_dir_scss = './dist/css-compiled';
-
-function build() {
-  return gulp.src(src_dir_scss)
-    .pipe(sourcemaps.init())
-    .pipe(sass({
-      outputStyle: 'compact',
-      precision: 10
-    }).on('error', sass.logError))
-    .pipe(sourcemaps.write())
-    .pipe(autoprefixer())
-    .pipe(csscomb())
-    .pipe(cleancss())
-    .pipe(rename({
-      suffix: '.min'
-    }))
-    .pipe(gulp.dest(dest_dir_scss))
+function compileSass() {
+  return gulp
+    .src(paths.src.scss)
+    .pipe(
+      sass({
+        outputStyle: 'expanded',
+        precision: 10,
+      }).on('error', sass.logError)
+    )
+    .pipe(gulp.dest(paths.dest.css))
     .pipe(browserSync.stream());
 }
 
-gulp.task('images', () => {
-  return gulp.src(src_dir_images)
-  .pipe(webp())
-  .pipe(gulp.dest('./dist/images-compiled'));
+gulp.task('convertImages', () => {
+  return gulp.src(paths.src.images).pipe(webp()).pipe(gulp.dest(paths.dest.images));
+});
+
+gulp.task('clean', () => {
+  return del(['dist/**/*']);
 });
 
 function watch() {
-    browserSync.init({
-        proxy: "localhost:8080"
-    });
-    gulp.watch(watch_dir_twig).on("change", reload);
-    gulp.watch(watch_dir_js).on("change", reload);
-  return gulp.watch(watch_dir_scss, build);
+  browserSync.init({
+    proxy: 'localhost:8080'
+  });
+  gulp.watch(paths.watch.scss, gulp.series('clean', compileSass));
+  gulp.watch(paths.watch.images, gulp.series('clean', 'convertImages'));
+  gulp.watch(paths.watch.twig).on('change', browserSync.reload);
+  gulp.watch(paths.watch.js).on('change', browserSync.reload);
 }
 
 exports.watch = watch;
-exports.build = build;
+exports.build = gulp.series('clean', compileSass, 'convertImages');
+exports.default = watch;
